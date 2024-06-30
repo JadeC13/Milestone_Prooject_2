04d97c3a962e548965bfbe2442da1eb2cef4cc6d
RaymanCarney <Raymancarney@gmail.com>
2024-06-29 20:03:06 -0400

    Leaderboard sorts out top scores and is on the main page need to add a router to have it show up on a different page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import Board from './components/leaderboard';
 import React from 'react';
+
 
 const App = () => {
     return (
         <div>
             <h1>Hello, It's my first React App!</h1>
             <h2>Happy Coding!!</h2>
+            <div className='Leaderboard'id='main'>
+                <Board />
+            </div>
         </div>
     );
 };
