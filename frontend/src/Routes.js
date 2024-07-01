734fb83008206f5a434c80058620752b7da9d34e
twedty <jamestwedt@gmail.com>
2024-07-01 19:32:05 -0400

    moved questions.js into source. Moved the text into database and linked it. Added the radio buttons and formatas well as timer

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Board from './components/leaderboard';
 import HomePage from './App';
+import QuizApp from './components/questions';
 
 export const Routes = () => {
     return (
@@ -11,6 +12,9 @@ export const Routes = () => {
                 </Route>
                 <Route path='/leaderboard'>
                     <Board />
+                </Route>
+                <Route path='/frontend/src/components/questions.js'>
+                    <QuizApp />
                 </Route>
             </Switch>
         </Router>
