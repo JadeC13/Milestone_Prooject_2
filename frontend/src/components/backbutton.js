926de1d6a4973848081f439bdfe324132dbd7b38
RaymanCarney <Raymancarney@gmail.com>
2024-07-01 21:47:56 -0400

    fixed issue with back button

--- frontend/src/components/backbutton.js
+++ frontend/src/components/backbutton.js
@@ -1,17 +1,25 @@
-import { useHistory } from 'react-router-dom';
+import HomePage from '../App';
 import './css/style.css'
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import React from 'react';
 
 const BackButton = () => {
-    const history = useHistory();
-
-    const goBack = () => {
-        history.goBack();
-    };
-
     return (
-        <button className="back-button" onClick={goBack}>Home</button>
+        <Link className="back-button"to="/">Home</Link>
     );
 };
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+            </Switch>
+            <BackButton />
+        </Router>
+    );
+};
+
 // This code above lets you use the back button to return to the Home Page
 
 export default BackButton;
